ef76a7ed530b0376064893dee9f839bb61438803
Jackson <jackson@jacksonmoody.com>
2024-01-13 02:58:10 -0500

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -7,7 +8,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Jackson Moody',
+  title: "Jackson Moody",
   description: "Jackson Moody's Personal Website",
   openGraph: {
     title: "Jackson Moody",
@@ -16,22 +17,23 @@ export const metadata: Metadata = {
     siteName: "Jackson Moody",
     images: [
       {
-        url: "https://drive.usercontent.google.com/download?id=11SQzHKN2kfckajwbsH8ktPuj30sS6Wpu&export=view&authuser=0"
+        url: "https://drive.usercontent.google.com/download?id=11SQzHKN2kfckajwbsH8ktPuj30sS6Wpu&export=view&authuser=0",
       },
     ],
     locale: "en_US",
     type: "website",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={"bg-slate-900"}>{children}</body>
+      <Analytics />
     </html>
-  )
+  );
 }
