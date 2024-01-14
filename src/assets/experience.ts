c3cce706abea8e2bf045c303cdf1f1f809b3a449
Jackson <jackson@jacksonmoody.com>
2024-01-14 02:05:37 -0500

    Update Experiences

--- src/assets/experience.ts
+++ src/assets/experience.ts
@@ -6,7 +6,7 @@ export const experience = [
     description: [
       "Created new template for long-form government web content—publishing 300+ pages of new material with vastly improved performance and accessibility.",
       "Led automation of data reconciliation project saving an estimated 350+ hours annually.",
-      "Selected for Coding It Forward Civic Digital Fellowship (4% acceptance rate)",
+      "Selected for Coding It Forward Civic Digital Fellowship (4% acceptance rate).",
     ],
     skills: ["Python", "HTML", "CSS", "Javascript", "Hugo"],
     link: "https://digital.gov/guides/hcd/",
@@ -17,7 +17,7 @@ export const experience = [
     company: "Formlabs",
     description: [
       "Led growth of the Formlabs Access Initiative for underserved students, now implemented successfully nationwide.",
-      "Resolved 100+ support tickets regarding complex issues in SLA 3D printing technology",
+      "Resolved 100+ support tickets regarding complex issues in SLA 3D printing technology.",
     ],
     skills: ["Leadership", "Communication", "Technical Support"],
     link: "https://formlabs.com",
@@ -28,7 +28,7 @@ export const experience = [
     company: "Intelligent Robotics Laboratory, University of Colorado",
     description: [
       "Trained lightweight neural networks to be used in prosthetic hand research across the University.",
-      "Pioneered the usage of “finger joints” in dual-material 3D printed soft actuators to increase the adhesion between rigid and flexible material types",
+      "Pioneered the usage of “finger joints” in dual-material 3D printed soft actuators to increase the adhesion between rigid and flexible material types.",
     ],
     skills: ["Tensorflow", "Python"],
     link: "https://www.colorado.edu/lab/correll/",
