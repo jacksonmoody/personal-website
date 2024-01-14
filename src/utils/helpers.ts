f8561d21d6a6da0e3153847330c82c5fbbf18b9d
Jackson <jackson@jacksonmoody.com>
2024-01-14 02:02:41 -0500

    Add UI Library

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,13 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 export function scrollTo(id: string) {
-  return (ev: React.MouseEvent<HTMLElement, MouseEvent>) => {
+  return (ev: React.MouseEvent<HTMLElement, MouseEvent> | React.FocusEvent) => {
     ev.preventDefault();
     document.querySelector(id)?.scrollIntoView({ behavior: "smooth" });
   };
+}
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,15 +9,53 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         primary: "#7BD2F8",
         secondary: "#dee4f8",
         tertiary: "#8892b0",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
   plugins: [
+    require("tailwindcss-animate"),
     plugin(function ({ addVariant }: { addVariant: Function }) {
       addVariant("hocus", ["&:hover", "&:focus"]);
     }),
